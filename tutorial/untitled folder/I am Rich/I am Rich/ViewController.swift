7b29429cb3326421b6340d5d2dd6cf849c4c8545
lawlietstudio <62687629+lawlietstudio@users.noreply.github.com>
2022-07-26 19:31:34 +0800

    add i am rich

---
+++ tutorial/untitled folder/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mark Ho on 27/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
