b57e852306aa407c014f7c6c21e8ff28b9ad3915
lawlietstudio <62687629+lawlietstudio@users.noreply.github.com>
2022-08-23 04:13:04 +0800

    add detailView

---
+++ tutorial/swift/HackerNews/HackerNews/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Mark Ho on 23/8/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "http://www.google.com")
+    }
+}
