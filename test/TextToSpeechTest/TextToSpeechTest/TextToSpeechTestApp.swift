95c98fb312636998c61e41b2d724cda37c3b3882
Mark Ho <mark.ho@tiffany.com>
2024-02-13 21:56:57 -0500

    no message

---
+++ test/TextToSpeechTest/TextToSpeechTest/TextToSpeechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechTestApp.swift
+//  TextToSpeechTest
+//
+//  Created by Mark Ho on 18/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
